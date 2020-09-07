cde8f6903a1218fd7f53486e6b0ff1e92970de55
Wilfried OLLIVIER <wollivier@bearstech.com>
2020-09-07 17:16:30 +0200

    Add owner package

---
+++ owner/owner.go
@@ -0,0 +1,52 @@
+package owner
+
+import (
+	"context"
+	"errors"
+)
+
+type contextKey string
+
+var (
+	userKey = contextKey("owner")
+)
+
+// Owner represents an authenticated user info
+type Owner struct {
+	Name  string
+	Admin bool
+}
+
+// ToCtx creates a context containing a user key
+func (u *Owner) ToCtx(in context.Context) context.Context {
+	return context.WithValue(in, userKey, *u)
+}
+
+// FromJWT creates a user if one is found in JWT claims
+func FromJWT(claims map[string]interface{}) (*Owner, error) {
+
+	val, ok := claims["owner"]
+	if !ok {
+		return nil, errors.New("Missing owner in JWT claims")
+	}
+
+	name, ok := val.(string)
+	if !ok {
+		return nil, errors.New("JWT owner claim is not a string")
+	}
+
+	return &Owner{
+		Name: name,
+	}, nil
+
+}
+
+// FromCtx extract a user from a context
+func FromCtx(ctx context.Context) (*Owner, error) {
+	u, ok := ctx.Value(userKey).(Owner)
+	if !ok {
+		return nil, errors.New("No user in this context")
+	}
+
+	return &u, nil
+}
