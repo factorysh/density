5a038a7ef0c7c53449314a5bfa72d331bdeadb95
Wilfried OLLIVIER <wollivier@bearstech.com>
2020-09-22 15:15:03 +0200

    Ensure docker-compose bin is available in PATH

--- action/compose.go
+++ action/compose.go
@@ -1,6 +1,15 @@
 package action
 
-import "context"
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+
+	"github.com/pkg/errors"
+)
 
 // Compose is a docker-compose project
 type Compose struct {
@@ -12,4 +21,27 @@ type Compose struct {
 // Action run the project
 func (c *Compose) Action(ctx context.Context) error {
 	return nil
+}
+
+// EnsureBin will ensure that docker-compose is found in $PATH
+func EnsureBin() error {
+	var name = "docker-compose"
+	var out bytes.Buffer
+
+	cmd := exec.Command("whereis", "-b", name)
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	sanitized := strings.TrimRight(out.String(), "\n")
+	matched, err := regexp.Match(fmt.Sprintf("%s: .*/%s", name, name), []byte(sanitized))
+	if !matched {
+		return errors.Errorf("Executable %s not found", name)
+	}
+
+	return nil
+
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/mux v1.8.0
+	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.6.0
 	github.com/stretchr/testify v1.6.1
 )
