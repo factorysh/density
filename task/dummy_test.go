7d98724bcc610696561e2fea818b4ec93c6ae890
Mathieu Lecarme <mlecarme@bearstech.com>
2020-12-16 17:14:42 +0100

    Waiter test.

--- task/dummy_test.go
+++ task/dummy_test.go
@@ -2,7 +2,9 @@ package task
 
 import (
 	"encoding/json"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -20,4 +22,19 @@ func TestDummyJson(t *testing.T) {
 	assert.NoError(t, err)
 	a.Wg.Done()
 	a.Wg.Wait()
+}
+
+func TestWaiter(t *testing.T) {
+	w := NewWaiter()
+	assert.Equal(t, 0, w.cpt)
+	w.Add(10)
+	assert.Equal(t, 10, w.cpt)
+	for i := 0; i < 10; i++ {
+		go func() {
+			time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+			w.Done()
+		}()
+	}
+	w.Wait()
+	assert.Equal(t, 0, w.cpt)
 }
