af1ab2bae8e51cf0834f7711efc1da008dc8f4c9
Mathieu Lecarme <mlecarme@bearstech.com>
2020-12-16 21:13:28 +0100

    Pubsub.

---
+++ scheduler/pubsub.go
@@ -0,0 +1,55 @@
+package scheduler
+
+import (
+	"context"
+	"sync"
+)
+
+type Event struct {
+}
+
+type PubSub struct {
+	lock        *sync.Mutex
+	cpt         uint64
+	subscribers map[uint64]chan Event
+	wg          *sync.WaitGroup
+}
+
+func NewPubSub() *PubSub {
+	return &PubSub{
+		lock:        &sync.Mutex{},
+		cpt:         0,
+		subscribers: make(map[uint64]chan Event),
+		wg:          &sync.WaitGroup{},
+	}
+}
+
+func (p *PubSub) Subscribe(ctx context.Context) chan Event {
+	p.lock.Lock()
+	id := p.cpt
+	p.cpt++
+	p.subscribers[id] = make(chan Event)
+	p.wg.Add(1)
+	p.lock.Unlock()
+	go func() {
+		<-ctx.Done()
+		p.lock.Lock()
+		delete(p.subscribers, id)
+		p.wg.Done()
+		p.lock.Unlock()
+	}()
+	return p.subscribers[id]
+}
+
+func (p *PubSub) Publish(evt Event) {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+	// Warning, chans are blocking
+	for _, c := range p.subscribers {
+		c <- evt
+	}
+}
+
+func (p *PubSub) Wait() {
+	p.wg.Wait()
+}
---
+++ scheduler/pubsub_test.go
@@ -0,0 +1,40 @@
+package scheduler
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPubsub(t *testing.T) {
+	ps := NewPubSub()
+	wg := sync.WaitGroup{}
+	size := 10
+	wg.Add(size)
+	cancels := make([]context.CancelFunc, 0)
+	for i := 0; i < size; i++ {
+		ctx, cancel := context.WithCancel(context.TODO())
+		cancels = append(cancels, cancel)
+		events := ps.Subscribe(ctx)
+
+		go func() {
+			for {
+				event := <-events
+				fmt.Println(event)
+				wg.Done()
+			}
+		}()
+	}
+	assert.Len(t, ps.subscribers, size)
+	ps.Publish(Event{})
+	wg.Wait()
+
+	for _, cancel := range cancels {
+		cancel()
+	}
+	ps.Wait()
+	assert.Len(t, ps.subscribers, 0)
+}
