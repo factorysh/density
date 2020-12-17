2ef9828ac84ea75c92c1470b74fdf1b38b6a3615
Mathieu Lecarme <mlecarme@bearstech.com>
2020-12-17 22:58:39 +0100

    Lock all the things.

--- store/memory.go
+++ store/memory.go
@@ -1,16 +1,22 @@
 package store
 
+import "sync"
+
 type MemoryStore struct {
-	kv map[string][]byte
+	kv   map[string][]byte
+	lock *sync.RWMutex
 }
 
 func NewMemoryStore() *MemoryStore {
 	return &MemoryStore{
-		kv: make(map[string][]byte),
+		kv:   make(map[string][]byte),
+		lock: &sync.RWMutex{},
 	}
 }
 
 func (m *MemoryStore) Get(key []byte) ([]byte, error) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 	v, ok := m.kv[string(key)]
 	if !ok {
 		return nil, nil
@@ -19,20 +25,28 @@ func (m *MemoryStore) Get(key []byte) ([]byte, error) {
 }
 
 func (m *MemoryStore) Put(key []byte, value []byte) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
 	m.kv[string(key)] = value
 	return nil
 }
 
 func (m *MemoryStore) Delete(key []byte) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
 	delete(m.kv, string(key))
 	return nil
 }
 
 func (m *MemoryStore) Length() int {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 	return len(m.kv)
 }
 
 func (m *MemoryStore) ForEach(fn func(k, v []byte) error) error {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
 	for k, v := range m.kv {
 		err := fn([]byte(k), v)
 		if err != nil {
