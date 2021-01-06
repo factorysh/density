633d01e5fcebea7a3793129ff39eb6af44e2525b
Mathieu Lecarme <mlecarme@bearstech.com>
2021-01-06 14:03:55 +0100

    More doc.

---
+++ compose/doc.go
@@ -0,0 +1,9 @@
+package compose
+
+/*
+compose package implements with docker-compose :
+ * task.Action
+ * task.Run
+
+It reads docker-compose.yml file, checks availability of `docker-compose` command.
+*/
---
+++ runner/doc.go
@@ -0,0 +1,9 @@
+package runner
+
+/*
+Runner is the runtime for the groupe of containers
+ * [x] docker-compose
+ * [ ] nomad
+ * [ ] kubernetes
+ * …
+*/
