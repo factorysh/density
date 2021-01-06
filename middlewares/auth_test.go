926c31951c84c47474286d6cf0ff31c75dc3d95b
Mathieu Lecarme <mlecarme@bearstech.com>
2021-01-06 14:44:09 +0100

    Testing bad signature.

--- middlewares/auth_test.go
+++ middlewares/auth_test.go
@@ -26,27 +26,38 @@ func TestAuth(t *testing.T) {
 
 	type fixture struct {
 		claim  jwt.MapClaims
+		key    []byte
 		status int
 	}
 	for _, a := range []fixture{
-		{
+		{ // it's ok
 			claim: jwt.MapClaims{
 				"owner": "bob",
 				"nbf":   time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
 			},
+			key:    []byte(key),
 			status: 200,
 		},
-		{
+		{ // owner is missing
 			claim: jwt.MapClaims{
 				"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
 			},
+			key:    []byte(key),
 			status: 400,
+		},
+		{ // wrong key
+			claim: jwt.MapClaims{
+				"owner": "bob",
+				"nbf":   time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+			},
+			key:    []byte("wrong key"),
+			status: 401,
 		},
 	} {
 		r, err := http.NewRequest("GET", ts.URL, nil)
 		assert.NoError(t, err)
 		token := jwt.NewWithClaims(jwt.SigningMethodHS256, a.claim)
-		blob, err := token.SignedString([]byte(key))
+		blob, err := token.SignedString(a.key)
 		assert.NoError(t, err)
 		r.Header.Set("Authorization", fmt.Sprintf("Bearer %s", blob))
 		res, err = client.Do(r)
