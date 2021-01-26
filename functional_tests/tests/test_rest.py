e18cf19709edc1020c50e4a8d6268aae94b333c0
Mathieu Lecarme <mlecarme@bearstech.com>
2021-01-26 22:38:06 +0100

    Test home.

---
+++ functional_tests/tests/test_rest.py
@@ -0,0 +1,23 @@
+import requests
+from pytest import fixture
+import jwt
+
+
+@fixture
+def session():
+    s = requests.Session()
+    s.headers.update({
+        'Authorization': 'Bearer %s' % jwt.encode(dict(owner="alice"),
+                                                  "s3cr3t", algorithm="HS256"),
+    })
+    return s
+
+
+def test_home(session):
+    r = session.get('http://localhost:8042')
+    assert r.status_code == 404
+
+
+def test_schedules(session):
+    r = session.get('http://localhost:8042/api/schedules')
+    assert r.status_code == 200
