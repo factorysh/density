6c54541cd02b39dfdab4da2d99a23dbf7bbd3671
Mathieu Lecarme <mlecarme@bearstech.com>
2021-02-03 16:14:48 +0100

    Test with JSON.

--- functional_tests/tests/test_rest.py
+++ functional_tests/tests/test_rest.py
@@ -6,27 +6,53 @@ import jwt
 @fixture
 def session():
     s = requests.Session()
-    s.headers.update({
-        'Authorization': 'Bearer %s' % jwt.encode(dict(owner="alice"),
-                                                  "s3cr3t", algorithm="HS256"),
-    })
+    s.headers.update(
+        {
+            "Authorization": "Bearer %s"
+            % jwt.encode(dict(owner="alice"), "s3cr3t", algorithm="HS256"),
+        }
+    )
     return s
 
 
 def test_home(session):
-    r = session.get('http://localhost:8042')
+    r = session.get("http://localhost:8042")
     assert r.status_code == 404
 
 
 def test_schedules(session):
-    r = session.get('http://localhost:8042/api/schedules')
+    r = session.get("http://localhost:8042/api/schedules")
     assert r.status_code == 200
-    r = session.post('http://localhost:8042/api/schedules', files={
-        'docker-compose': '''
+    r = session.post(
+        "http://localhost:8042/api/schedules",
+        files={
+            "docker-compose": """
 version: '3'
 services:
   hello:
     image: "busybox:latest"
     command: "echo world"
-        '''})
+        """
+        },
+    )
     assert r.status_code == 200
+
+
+def test_json(session):
+    r = session.post(
+        "http://localhost:8042/api/schedules",
+        json={
+            "cpu": 2,
+            "ram": 128,
+            "max_execution_time": "120s",
+            "action": {
+                "compose": {
+                    "version": "3",
+                    "services": {
+                        "hello": {"image": "busybox:latest", "command": "echo World"}
+                    },
+                }
+            },
+        },
+    )
+    assert r.status_code == 201
