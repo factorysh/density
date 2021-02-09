7d49aae555b1f8b7365e0c9715ff75dcbd0eea4e
Wilfried OLLIVIER <wollivier@bearstech.com>
2021-02-09 13:58:34 +0100

    Add wait for param

--- functional_tests/tests/test_rest.py
+++ functional_tests/tests/test_rest.py
@@ -68,41 +68,52 @@ def test_json(session):
 
 
 def test_prune_on_cancel(session):
-    r = session.get("http://localhost:8042/api/schedules")
-    assert r.status_code == 200
-    r = session.post(
-        "http://localhost:8042/api/schedules",
-        files={
-            "docker-compose": """
+    testcases = [{"name": "without wait for",
+                  "status": 202, "wait_for": False}]
+
+    for case in testcases:
+        r = session.get("http://localhost:8042/api/schedules")
+        assert r.status_code == 200
+        r = session.post(
+            "http://localhost:8042/api/schedules",
+            files={
+                "docker-compose":
+                """
 version: '3'
 services:
-  hello:
-    image: "busybox:latest"
-    command: "sh -c 'sleep 2 && echo world'"
+    hello:
+        image: "busybox:latest"
+        command: "sh -c 'sleep 2 && echo world'"
 x-batch:
-  max_execution_time: 3s
-        """
-        },
-    )
+    max_execution_time: 3s
+"""
+            },
+        )
 
-    assert r.status_code == 201
-    resp = json.loads(r.text)
-    id = resp["id"]
+        assert r.status_code == 201, "status error in test %s" % case["name"]
+        resp = json.loads(r.text)
+        id = resp["id"]
 
-    cli = docker.from_env()
-    tries = 5
-    for tick in range(0, tries):
-        try:
-            time.sleep(0.5)
+        cli = docker.from_env()
+        tries = 5
+        for tick in range(0, tries):
+            try:
+                time.sleep(0.5)
+                ct = cli.containers.get("%s_hello_1" % id)
+                break
+            except Exception as e:
+                if tick == tries - 1:
+                    raise Exception("Can't find container")
+                print(e)
+
+        time.sleep(1)
+
+        url = "http://localhost:8042/api/schedules/%s" % id
+        if case["wait_for"]:
+            url = "%s?wait_for" % url
+
+        r = session.delete(url)
+        assert r.status_code == case["status"], "status error in test %s" % case["name"]
+        time.sleep(2)
+        with raises(docker.errors.NotFound):
             ct = cli.containers.get("%s_hello_1" % id)
-            break
-        except Exception as e:
-            if tick == tries - 1:
-                raise Exception("Can't find container")
-            print(e)
-
-    time.sleep(1)
-    r = session.delete(
-        "http://localhost:8042/api/schedules/%s" % id
-    )
-    assert r.status_code == 202
