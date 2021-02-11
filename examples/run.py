0f725e17dfe911a77e4d0e086fe26a27cbe93fad
Wilfried OLLIVIER <wollivier@bearstech.com>
2021-02-11 15:52:23 +0100

    Add examples

---
+++ examples/run.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import jwt
+
+
+# create fresh session with header token
+def session():
+    s = requests.Session()
+    s.headers.update(
+        {
+            "Authorization": "Bearer %s"
+            % jwt.encode(dict(owner="alice"), "s3cr3t", algorithm="HS256"),
+        }
+    )
+    return s
+
+
+# run job using provided session
+def run(session):
+    r = session.get("http://localhost:8042/api/schedules")
+    assert r.status_code == 200
+
+    with open("./compose/sitespeed-compose.yml", "r", encoding="utf-8") as compose:
+        raw = compose.read()
+        print(raw)
+
+    r = session.post(
+        "http://localhost:8042/api/schedules",
+        files={"docker-compose": raw},
+    )
+
+    assert r.status_code == 201
+
+
+run(session())
