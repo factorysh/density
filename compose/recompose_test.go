e397fa4d6ee628f0049636c0f8b332dd813c8c99
Mathieu Lecarme <mlecarme@bearstech.com>
2021-02-22 23:11:28 +0100

    Testing recompose.

---
+++ compose/recompose_test.go
@@ -0,0 +1,32 @@
+package compose
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/docker/docker/client"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
+)
+
+func TestRecompose(t *testing.T) {
+	c := NewCompose()
+	err := yaml.Unmarshal([]byte(`
+version: '3'
+services:
+  hello:
+    image: "busybox:latest"
+    command: "echo world"
+x-batch:
+  key: value
+`), c)
+	assert.NoError(t, err)
+	docker, err := client.NewEnvClient()
+	assert.NoError(t, err)
+	composator := NewRecomposator(docker)
+	prod, err := composator.Recompose("bob", c)
+	assert.NoError(t, err)
+	out, err := yaml.Marshal(prod)
+	assert.NoError(t, err)
+	fmt.Println(string(out))
+}
