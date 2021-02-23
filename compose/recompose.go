f2d30656c81b197e62f64c97445dc47ff04d99da
Mathieu Lecarme <mlecarme@bearstech.com>
2021-02-23 11:08:06 +0100

    more CIDR.

--- compose/recompose.go
+++ compose/recompose.go
@@ -2,10 +2,11 @@ package compose
 
 import (
 	"context"
-	"errors"
 	"fmt"
+	"net"
 	"sync"
 
+	"github.com/apparentlymart/go-cidr/cidr"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
@@ -13,44 +14,50 @@ import (
 )
 
 type Recomposator struct {
-	docker *client.Client
-	used   []uint32
-	lock   *sync.Mutex
+	docker  *client.Client
+	lastNet *net.IPNet
+	lock    *sync.Mutex
 }
 
 func NewRecomposator(docker *client.Client) *Recomposator {
 	return &Recomposator{
 		docker: docker,
-		used:   make([]uint32, 0),
-		lock:   &sync.Mutex{},
+		lastNet: &net.IPNet{
+			IP:   net.ParseIP("172.16.0.0"),
+			Mask: net.CIDRMask(22, 32),
+		},
+		lock: &sync.Mutex{},
 	}
 }
 
-func (r *Recomposator) newSubnet() (uint32, error) {
+func (r *Recomposator) newSubnet() (*net.IPNet, bool) {
+	networks, err := r.docker.NetworkList(context.TODO(), types.NetworkListOptions{})
+	if err != nil {
+		fmt.Println(err)
+		return nil, false
+	}
+	fmt.Println(networks)
+	for _, network := range networks {
+		for _, config := range network.IPAM.Config {
+			fmt.Println(config.Subnet)
+		}
+	}
 	// FIXME yeah, it's ugly
 	r.lock.Lock()
 	defer r.lock.Unlock()
-	if len(r.used) == 0 {
-		r.used = []uint32{0}
-		return 0, nil
-	}
-	n := r.used[len(r.used)-1] + 12
-	if n > 254 {
-		return 0, errors.New("Subnet exhausting")
-	}
-	r.used = append(r.used, n)
-	return n, nil
+	n, ok := cidr.NextSubnet(r.lastNet, 10)
+	return n, ok
 }
 
 // Recompose take a naive and validated Compose and return a Compose as it will be run
 func (r *Recomposator) Recompose(name string, c *Compose) (*Compose, error) {
 	networkID := uuid.New()
-	n, err := r.newSubnet()
-	if err != nil {
-		return nil, err
+	n, ok := r.newSubnet()
+	if !ok {
+		return nil, fmt.Errorf("Can't create a new subnet %v", r.lastNet)
 	}
 	networkName := fmt.Sprintf("batch-%s-%s", name, networkID.String())
-	_, err = r.docker.NetworkCreate(context.TODO(), networkName, types.NetworkCreate{
+	_, err := r.docker.NetworkCreate(context.TODO(), networkName, types.NetworkCreate{
 		CheckDuplicate: true,
 		EnableIPv6:     false,
 		Scope:          "local",
@@ -63,8 +70,8 @@ func (r *Recomposator) Recompose(name string, c *Compose) (*Compose, error) {
 			Driver: "default",
 			Config: []network.IPAMConfig{
 				{
-					Subnet:  fmt.Sprintf("172.16.%d.0/22", n),
-					Gateway: fmt.Sprintf("172.16.%d.1", n),
+					Subnet:  n.IP.String(),
+					Gateway: n.IP.String(),
 				},
 			},
 		},
