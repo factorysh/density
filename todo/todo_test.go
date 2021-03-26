7521c5b28f3be567d719963e972a509333125145
Mathieu Lecarme <mlecarme@bearstech.com>
2021-03-26 21:28:21 +0100

    Draft.

---
+++ todo/todo_test.go
@@ -0,0 +1,30 @@
+package todo
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestTodo(t *testing.T) {
+	todo := New()
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		for {
+			select {
+			case <-todo.Wait():
+				time.Sleep(10 * time.Millisecond)
+				todo.Done()
+				wg.Done()
+			case <-time.After(10 * time.Second):
+			}
+		}
+	}()
+	todo.Ping()
+	todo.Ping()
+	todo.Ping()
+	time.Sleep(20 * time.Millisecond)
+	todo.Ping()
+	wg.Wait()
+}
