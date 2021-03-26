0348cd329a12b0c7bbd54cc7fe4a1a51831c61be
Mathieu Lecarme <mlecarme@bearstech.com>
2021-03-26 21:56:59 +0100

    Full test coverage.

--- todo/todo.go
+++ todo/todo.go
@@ -5,12 +5,14 @@ import (
 	"sync"
 )
 
+// Todo is a todo list
 type Todo struct {
 	newMsg bool
 	lock   sync.Mutex
 	wait   chan interface{}
 }
 
+// New Todo
 func New() *Todo {
 	return &Todo{
 		newMsg: false,
@@ -19,28 +21,33 @@ func New() *Todo {
 	}
 }
 
-func (t *Todo) Ping() {
+// Ping something happened in your todo list
+func (t *Todo) Ping() bool {
 	t.lock.Lock()
 	defer t.lock.Unlock()
 	if !t.newMsg {
 		t.wait <- new(interface{})
 		t.newMsg = true
+		return true
 	}
+	return false
 }
 
+// Done tell that you finished your todo list
 func (t *Todo) Done() error {
 	t.lock.Lock()
 	defer t.lock.Unlock()
-	t.newMsg = false
 	if !t.newMsg {
 		return errors.New("double release")
 	}
+	t.newMsg = false
 	if len(t.wait) > 0 { // flushing the chan
 		<-t.wait
 	}
 	return nil
 }
 
+// Wait for the next ping
 func (t *Todo) Wait() chan interface{} {
 	return t.wait
 }
--- todo/todo_test.go
+++ todo/todo_test.go
@@ -4,6 +4,8 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTodo(t *testing.T) {
@@ -15,17 +17,37 @@ func TestTodo(t *testing.T) {
 			select {
 			case <-todo.Wait():
 				time.Sleep(10 * time.Millisecond)
-				todo.Done()
+				err := todo.Done()
+				assert.NoError(t, err)
 				wg.Done()
 			case <-time.After(10 * time.Second):
 				panic("Timeout")
 			}
 		}
 	}()
-	todo.Ping()
-	todo.Ping()
-	todo.Ping()
+	ok := todo.Ping()
+	assert.True(t, ok)
+	ok = todo.Ping()
+	assert.False(t, ok)
+	ok = todo.Ping()
+	assert.False(t, ok)
 	time.Sleep(20 * time.Millisecond)
-	todo.Ping()
+	ok = todo.Ping()
+	assert.True(t, ok)
 	wg.Wait()
+}
+
+func TestDone(t *testing.T) {
+	todo := New()
+	err := todo.Done()
+	assert.Error(t, err)
+}
+
+func TestFlush(t *testing.T) {
+	todo := New()
+	todo.Ping()
+	assert.Len(t, todo.wait, 1)
+	err := todo.Done()
+	assert.NoError(t, err)
+	assert.Len(t, todo.wait, 0)
 }
