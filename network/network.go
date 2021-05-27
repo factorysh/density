5fc3b6d4758bcad64c2f6326857df2c0c29093d9
Mathieu Lecarme <mlecarme@bearstech.com>
2021-05-27 22:04:47 +0200

    Optimist next available network.

--- network/network.go
+++ network/network.go
@@ -3,23 +3,59 @@ package network
 import (
 	"encoding/binary"
 	"net"
+	"sort"
 )
 
 type ByNetwork []*net.IPNet
 
 func (a ByNetwork) Len() int           { return len(a) }
 func (a ByNetwork) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByNetwork) Less(i, j int) bool { return IP4Less(a[i].IP, a[j].IP) }
+func (a ByNetwork) Less(i, j int) bool { return ip4Less(a[i].IP, a[j].IP) }
 
-func IP4Less(a, b net.IP) bool {
-	return binary.LittleEndian.Uint32(a.To4()) < binary.LittleEndian.Uint32(b.To4())
+func ip4Less(a, b net.IP) bool {
+	return binary.BigEndian.Uint32(a.To4()) < binary.BigEndian.Uint32(b.To4())
 }
 
+func intPow(n, m int) int {
+	if m == 0 {
+		return 1
+	}
+	result := n
+	for i := 2; i <= m; i++ {
+		result *= n
+	}
+	return result
+}
+
+// FirstLast returns the first and last IP of a network
 func FirstLast(network *net.IPNet) (net.IP, net.IP) {
-	first := binary.LittleEndian.Uint32(network.IP)
-	mask := binary.LittleEndian.Uint32(network.Mask)
+	first := binary.BigEndian.Uint32(network.IP)
+	mask := binary.BigEndian.Uint32(network.Mask)
 	last := (first & mask) | (0xffffffff - mask)
 	l := make([]byte, 4)
-	binary.LittleEndian.PutUint32(l, last)
+	binary.BigEndian.PutUint32(l, last)
 	return network.IP, net.IP(l)
+}
+
+func Distance(a, b net.IP) int {
+	return int(binary.BigEndian.Uint32(b) - binary.BigEndian.Uint32(a))
+}
+
+func NextAvailableNetwork(networks []*net.IPNet, mini *net.IPNet, max *net.IPNet, mask net.IPMask) (*net.IPNet, error) {
+	if len(networks) == 0 { // no other networks
+		return mini, nil
+	}
+	sort.Sort(ByNetwork(networks))
+	_, l := FirstLast(networks[len(networks)-1])
+	fMax, _ := FirstLast(max)
+	ones, _ := mask.Size()
+	d := Distance(l, fMax)
+	if d > intPow(2, (32-ones)) { // there is room in the queue
+		start := make([]byte, 4)
+		binary.BigEndian.PutUint32(start, binary.BigEndian.Uint32(l)+1)
+		return &net.IPNet{
+			IP:   start,
+			Mask: mask}, nil
+	}
+	return nil, nil
 }
--- network/network_test.go
+++ network/network_test.go
@@ -36,3 +36,26 @@ func TestLast(t *testing.T) {
 	assert.Equal(t, net.IP{172, 18, 1, 0}, first)
 	assert.Equal(t, net.IP{172, 18, 1, 255}, last)
 }
+
+func TestNext(t *testing.T) {
+
+	networks := make([]*net.IPNet, 0)
+	for _, cidr := range []string{
+		"172.18.1.0/24",
+		"172.17.1.0/24",
+		"172.18.2.0/24",
+	} {
+		_, n, err := net.ParseCIDR(cidr)
+		assert.NoError(t, err)
+		networks = append(networks, n)
+	}
+	_, min, err := net.ParseCIDR("172.17.0.0/24")
+	assert.NoError(t, err)
+	_, max, err := net.ParseCIDR("172.18.32.0/24")
+	assert.NoError(t, err)
+	next, err := NextAvailableNetwork(networks, min, max, net.IPMask{255, 255, 255, 0})
+	assert.NoError(t, err)
+	_, myNext, err := net.ParseCIDR("172.18.3.0/24")
+	assert.NoError(t, err)
+	assert.Equal(t, myNext, next)
+}
