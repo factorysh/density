c8a75d6f9f2bfcf489a9be8f2e76b4c56dd93b54
Mathieu Lecarme <mlecarme@bearstech.com>
2021-06-01 22:32:44 +0200

    find hole. More tests. netdistance is hand to hand.

--- network/network.go
+++ network/network.go
@@ -2,15 +2,19 @@ package network
 
 import (
 	"encoding/binary"
+	"errors"
+	"fmt"
 	"net"
 	"sort"
 )
 
 type ByNetwork []*net.IPNet
 
-func (a ByNetwork) Len() int           { return len(a) }
-func (a ByNetwork) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByNetwork) Less(i, j int) bool { return ip4Less(a[i].IP, a[j].IP) }
+func (a ByNetwork) Len() int      { return len(a) }
+func (a ByNetwork) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByNetwork) Less(i, j int) bool {
+	return ip4Less(a[i].IP, a[j].IP)
+}
 
 func ip4Less(a, b net.IP) bool {
 	return binary.BigEndian.Uint32(a.To4()) < binary.BigEndian.Uint32(b.To4())
@@ -46,7 +50,15 @@ func Distance(a, b net.IP) int {
 	return -int(ia - ib)
 }
 
-// NetDistance is absolute distance between 2 networks, if < 0 it's an overlap
+// NetDistance is absolute distance between 2 networks, last to last, if < 0 it's an overlap
+/*
+|--a--|  |--b--|
+      |<------>|
+
+|--a--|
+   |---b--|
+   |<->|
+*/
 func NetDistance(a, b *net.IPNet) int {
 	af, al := FirstLast(a)
 	iaf := binary.BigEndian.Uint32(af)
@@ -65,11 +77,11 @@ func NetDistance(a, b *net.IPNet) int {
 		// overlap
 		return -int(ial - ibf)
 	}
-	if iaf > ibl { // a is after bf
-		return int(iaf - ibl)
+	if iaf > ibl { // a is after b
+		return int(ial - ibl)
 	}
 	// b is after a
-	return int(ibf - ial)
+	return int(ibl - ial)
 }
 
 func nextNet(a *net.IPNet, mask net.IPMask) *net.IPNet {
@@ -81,34 +93,42 @@ func nextNet(a *net.IPNet, mask net.IPMask) *net.IPNet {
 		Mask: mask}
 }
 
-func NextAvailableNetwork(networks []*net.IPNet, mini *net.IPNet, max *net.IPNet, mask net.IPMask) (*net.IPNet, error) {
+func NextAvailableNetwork(networks []*net.IPNet,
+	mini *net.IPNet,
+	maxi *net.IPNet,
+	mask net.IPMask) (*net.IPNet, error) {
 	if len(networks) == 0 { // no other networks
 		return mini, nil
 	}
 	sort.Sort(ByNetwork(networks))
 	_, l := FirstLast(networks[len(networks)-1])
-	fMax, _ := FirstLast(max)
+	fMax, _ := FirstLast(maxi)
 	d := Distance(l, fMax)
 	ones, _ := mask.Size()
 	maskSize := intPow(2, (32 - ones))
 	if d > maskSize { // there is room in the queue
 		return nextNet(networks[len(networks)-1], mask), nil
-	} else {
-		poz := 0
-		n := mini
-		for { // FIXME don't loop for ever
-			d := NetDistance(n, networks[poz+1])
-			if d < 0 {
-				n = nextNet(n, mask)
-				poz++
-				continue
-			}
-			if d >= maskSize {
-				return n, nil
-			}
+	}
+	poz := 0
+	n := mini
+	for {
+		d := NetDistance(n, networks[poz])
+		if d < 0 {
 			n = nextNet(n, mask)
 			poz++
+			continue
+		}
+		if d >= maskSize {
+			return n, nil
+		}
+		n = nextNet(n, mask)
+		f, _ := FirstLast(n)
+		if Distance(fMax, f) > 0 {
+			return nil, fmt.Errorf("beyond max net : %v", maxi)
+		}
+		poz++
+		if poz == len(networks) {
+			return nil, errors.New("full network")
 		}
 	}
-	return nil, nil
 }
